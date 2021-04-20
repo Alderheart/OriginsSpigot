a654b6ed0adf38c565003a02d89d2e24e2a6c1a6
Mark <mooshroomTV@gmail.com>
2021-04-19 21:02:42 -0400

    Meow

--- Origins Spigot Wayward Project/src/me/xslate/originsspigot/avian/Effectevents.java
+++ Origins Spigot Wayward Project/src/me/xslate/originsspigot/avian/Effectevents.java
@@ -10,6 +10,9 @@ public class Effectevents implements Listener {
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
+		//Hello
+		
+		
 		//if(class = "Avian") {
 		event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, Integer.MAX_VALUE, 1, true, false));
 		event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0, true, false));
