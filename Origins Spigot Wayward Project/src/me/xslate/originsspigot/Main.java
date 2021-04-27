f93530a5d0a11d1741388666217b0c2458e20fce
BuildTools <unconfigured@null.spigotmc.org>
2021-04-26 20:06:26 -0600

    gui

--- Origins Spigot Wayward Project/src/me/xslate/originsspigot/Main.java
+++ Origins Spigot Wayward Project/src/me/xslate/originsspigot/Main.java
@@ -2,23 +2,23 @@ package me.xslate.originsspigot;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 import me.xslate.originsspigot.avian.EffectEvents;
 import me.xslate.originsspigot.avian.MeatEatingEvents;
 import me.xslate.originsspigot.avian.SleepEvents;
+import me.xslate.originsspigot.gui.Gui;
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
-	
-	//sky
+
 	@Override
 	public void onEnable() {
 		
 		getServer().getPluginManager().registerEvents(new SleepEvents(), this);
 		getServer().getPluginManager().registerEvents(new EffectEvents(), this);
 		getServer().getPluginManager().registerEvents(new MeatEatingEvents(), this);
+		
+		getCommand("class").setExecutor(new Gui());
 	
 	}
 public static final Pattern HEX = Pattern.compile("<#(\\w{5}[0-9a-f])>");
--- Origins Spigot Wayward Project/src/me/xslate/originsspigot/gui/Gui.java
+++ Origins Spigot Wayward Project/src/me/xslate/originsspigot/gui/Gui.java
@@ -12,46 +12,53 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import me.xslate.originsspigot.Main;
 
 public class Gui implements CommandExecutor {
 	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
-		if (sender instanceof Player) {
+	
+		if(sender instanceof Player) {
 			Player player = (Player) sender;
+		 
+				System.out.println("1");
 			
-			Inventory gui = Bukkit.createInventory(player, 27, ChatColor.DARK_GRAY +"Select Your Origin");
-			
-			ItemStack arrow = new ItemStack(Material.ARROW);
-			ItemMeta arrowmeta = arrow.getItemMeta();
-			arrowmeta.setDisplayName(Main.c("&7&oNext Page"));
-			ArrayList<String> arrowlore = new ArrayList<>();
-			arrowlore.add(Main.c("&7&oPage 2"));
-			arrowmeta.setLore(arrowlore);
-			arrow.setItemMeta(arrowmeta);
-			
-			ItemStack avian = new ItemStack(Material.FEATHER);
-			ItemMeta avianmeta = avian.getItemMeta();
-			avianmeta.setDisplayName(Main.c("&f&lAvian"));
-			ArrayList<String> avianlore = new ArrayList<>();
-			avianlore.add(Main.c("&7Permanent slow falling"));
-			avianlore.add(Main.c("&7Permanet Speed 1"));
-			avianlore.add(Main.c("&7Can only sleep at Y86 or higher"));
-			avianlore.add(Main.c("&7Vegetarin"));
-			avianmeta.setLore(avianlore);
-			avian.setItemMeta(avianmeta);
-			
-			
-			ItemStack[] menu_items = {null, null, null, null, null, null, null, null, null, null, null, null, null, avian, null, null, null, null, null, null, null, null,
-					null, null, null, null, arrow};
-			gui.setContents(menu_items);
-			player.openInventory(gui);
-			
-		}
+					Inventory gui = Bukkit.createInventory(player, 9, ChatColor.DARK_RED + "Server GUI");
+		
+					
+					ItemStack arrow = new ItemStack(Material.ARROW);
+					ItemStack avian = new ItemStack(Material.FEATHER);
+		
+		
+					ItemMeta arrowmeta = arrow.getItemMeta();
+					arrowmeta.setDisplayName(Main.c("&7&oNext Page"));
+					ArrayList<String> arrowlore = new ArrayList<>();
+					arrowlore.add(Main.c("&7&oPage 2"));
+					arrowmeta.setLore(arrowlore);
+					arrow.setItemMeta(arrowmeta);
+		
+		
+					ItemMeta avianmeta = avian.getItemMeta();
+					avianmeta.setDisplayName(Main.c("&f&lAvian"));
+					ArrayList<String> avianlore = new ArrayList<>();
+					avianlore.add(Main.c("&7Permanent slow falling"));
+					avianlore.add(Main.c("&7Permanet Speed 1"));
+					avianlore.add(Main.c("&7Can only sleep at Y86 or higher"));
+					avianlore.add(Main.c("&7Vegetarin"));
+					avianmeta.setLore(avianlore);
+					avian.setItemMeta(avianmeta);
+		
+					System.out.println("2");
+					ItemStack[] menu_items = {null, null};
+					gui.setContents(menu_items);
+					player.openInventory(gui);
+					System.out.println("3");
+				}	
+	
+		System.out.println("4");
 		return true;
 	}
 
-}
+	}
