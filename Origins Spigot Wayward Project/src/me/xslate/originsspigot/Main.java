ac3d32df4ed3d9757dbdfccb2318c8aaf87159ac
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 13:50:54 -0600

    Finished the WIP GUI to select your origin, all it needs now is the classes added in place of the palceholders.

--- Origins Spigot Wayward Project/src/me/xslate/originsspigot/Main.java
+++ Origins Spigot Wayward Project/src/me/xslate/originsspigot/Main.java
@@ -2,15 +2,20 @@ package me.xslate.originsspigot;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import me.xslate.originsspigot.avian.EffectEvents;
 import me.xslate.originsspigot.avian.MeatEatingEvents;
 import me.xslate.originsspigot.avian.SleepEvents;
-import me.xslate.originsspigot.gui.ClickEvents;
 import me.xslate.originsspigot.gui.StartGUI;
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
+
+	
+	
 
 	@Override
 	public void onEnable() {
@@ -19,8 +24,9 @@ public class Main extends JavaPlugin {
 		getServer().getPluginManager().registerEvents(new EffectEvents(), this);
 		getServer().getPluginManager().registerEvents(new MeatEatingEvents(), this);
 		
-		getCommand("gui").setExecutor(new StartGUI());
-		getServer().getPluginManager().registerEvents(new ClickEvents(), this);
+		getCommand("Origins").setExecutor(new StartGUI());
+		
+	
 	
 	}
 public static final Pattern HEX = Pattern.compile("<#(\\w{5}[0-9a-f])>");
--- Origins Spigot Wayward Project/src/me/xslate/originsspigot/gui/StartGUI.java
+++ Origins Spigot Wayward Project/src/me/xslate/originsspigot/gui/StartGUI.java
@@ -8,28 +8,132 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import me.xslate.originsspigot.Main;
 
 public class StartGUI implements CommandExecutor {
 	
-	@Override
+	
+	public Inventory inventory;
+
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
-		if (sender instanceof Player) {
+		if (label.equalsIgnoreCase("origins")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Only a player may execute this command!");
+				return true;
+			}
+			createInventory();
 			Player player = (Player) sender;
+			player.openInventory(inventory); // error here
+			return true;
 			
-			
-			
-			Inventory gui = Bukkit.createInventory(player, 27, Main.c("&b&lOrigins"));
-			
-			ItemStack placeholder = new ItemStack(Material.PAPER);
-			ItemStack avian = new ItemStack(Material.FEATHER);
-			ItemStack glass = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
-			
-			ItemStack[] menu_items = {glass, placeholder, null, placeholder, null, placeholder, null, placeholder, glass, glass, null, null, null, placeholder, null, null, null, glass, glass, placeholder, null, placeholder, null, placeholder, null, placeholder, glass};
-			gui.setContents(menu_items);
-			player.openInventory(gui);
-		} return true;
-	} 
+		}
+		return false;
+	}
+	
+	public void createInventory() {
+		
+		inventory = Bukkit.createInventory(null, 27, Main.c("&c&lOrigin"));
+		
+		ItemStack item = new ItemStack(Material.PAPER);
+		ItemMeta meta = item.getItemMeta();
+		
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName("");
+		item.setItemMeta(meta);
+		inventory.setItem(0, item);
+		
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(1, item);
+		
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(3, item);
+		
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(5, item);
+		
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(7, item);
+		
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName(null);
+		item.setItemMeta(meta);
+		inventory.setItem(8, item);
+		
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName(null);
+		item.setItemMeta(meta);
+		inventory.setItem(9, item);
+		
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(13, item);
+		
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName(null);
+		item.setItemMeta(meta);
+		inventory.setItem(17, item);
+		
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName(null);
+		item.setItemMeta(meta);
+		inventory.setItem(18, item);
+				
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(19, item);
+				
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(21, item);
+				
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(23, item);
+				
+		//Placeholder
+		item.setType(Material.PAPER);
+		meta.setDisplayName(Main.c("&5&oPlaceholder"));
+		item.setItemMeta(meta);
+		inventory.setItem(25, item);
+				
+		//Glass
+		item.setType(Material.BLACK_STAINED_GLASS_PANE);
+		meta.setDisplayName(null);
+		item.setItemMeta(meta);
+		inventory.setItem(26, item);
+		
+				
+				
+				
+	}
+	
+
 }
